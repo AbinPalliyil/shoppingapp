cd311d95286bcc96458eef7df482444398639e33
Abin <abin@luminescent.digital>
2020-06-13 14:01:06 +0530

    Creaate backend server

---
+++ backend/data.js
@@ -0,0 +1,39 @@
+export default  {
+	products: [
+		{
+			_id: 1,
+			name: 'slim shirt',
+			category: 'shirts',
+			brand: 'Nike',
+			imageUrl: '/images/shirtslim.jpg',
+			price: 1300,
+			rating: 4,
+			numReviews: 50,
+			description: "this is one of the coolest style in now a days"
+		},
+		{
+			_id: 2,
+            name: 'big shirt',
+            category: 'shirts',            
+			brand: 'Addidas',
+			imageUrl: '/images/shirtslim.jpg',
+			price: 1400,
+			rating: 4.5,
+			numReviews: 100,
+			description: "Linen type comfort fit and nice emprotary works"
+
+		},
+		{
+			_id: 3,
+			name: 'pencil fit',
+			category: 'pants',
+			brand: 'basics',
+			imageUrl: '/images/shirtslim.jpg',
+			price: 1200,
+			rating: 4,
+			numReviews: 10,
+			description: "Modern outfilts with different colors"
+
+		},
+	],
+};
